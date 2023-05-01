3b3a1398398f7275b587454e10aa6fb222442fbf
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2023-05-01 21:55:40 +0200

    change background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import {Inscription, Header} from '../src/Components/index'
 import './index.css'
+import {Info} from "./Components";
 
 function App() {
   return (
     <div>
         <Inscription />
         <Header />
+        <Info />
     </div>
   );
 }
